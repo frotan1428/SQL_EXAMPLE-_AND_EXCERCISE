bac55afdfbbf595ddfaa19e3c5a2aba78a1e1c8c
MFROTAN <98325864+frotan1428@users.noreply.github.com>
2022-04-11 09:12:50 +0430

    WHERE Claues

---
+++ 4-WHERE  Clause.sql
@@ -0,0 +1,17 @@
+SELECT * 
+FROM  customers
+WHERE birth_date!='1990-01-01';
+-- Coprator Oprator--
+-- >--
+-- >= --
+-- < --
+-- =< --
+-- == --
+-- != --
+-- -- put String inside single qoute -- 
+-- Excersie--
+-- get the orders placed this Year--
+SELECT * 
+FROM orders
+WHERE order_date >= '2019-01-1'
+
