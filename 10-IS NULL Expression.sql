ca540e7512697ec7d76fb45c9fa2869c82c618b5
MFROTAN <98325864+frotan1428@users.noreply.github.com>
2022-04-11 11:34:41 +0430

    IS NULL Clause

---
+++ 10-IS NULL Expression.sql
@@ -0,0 +1,9 @@
+-- IS NULL TO Ckeck THE NULL in The Table--
+
+SELECT * 
+FROM customers
+WHERE phone IS NULL;
+-- IS NOT NULL To Check For Not Null values in the tables
+SELECT * 
+FROM customers
+WHERE phone IS NOT  NULL;
